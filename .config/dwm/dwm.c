f3829b063826bc39acd0b2ba997e1aaf845daedf
Exyloss <antoninpons@protonmail.com>
2021-07-14 14:25:53 +0200

    ajout de togglefullscr dans dwm, modification de aliasrc et ajout de variables env.

--- .config/dwm/dwm.c
+++ .config/dwm/dwm.c
@@ -226,6 +226,7 @@ static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
 static void unmanage(Client *c, int destroyed);
 static void unmapnotify(XEvent *e);
@@ -1777,6 +1778,13 @@ togglefloating(const Arg *arg)
 		resize(selmon->sel, selmon->sel->x, selmon->sel->y,
 			selmon->sel->w, selmon->sel->h, 0);
 	arrange(selmon);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
--- .zshenv
+++ .zshenv
@@ -2,10 +2,6 @@
 # ~/.zshenv
 #
 
-export EDITOR="nvim"
-export TERMINAL="st"
-export BROWSER="brave"
-
 export PATH="$HOME/.local/bin"
 export SUDO_ASKPASS="$HOME/.local/bin/dpass"
 export XDG_CONFIG_HOME="$HOME/.config"
@@ -15,6 +11,10 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 export LANG=fr_FR.UTF-8
 export ZDOTDIR=$HOME/.config/zsh
+export READER="zathura"
+export EDITOR="nvim"
+export TRUEBROWSER="brave"
+export TERMINAL="urxvt"
 export LESSHISTFILE=-
 export WGETRC="${XDG_CONFIG_HOME:-$HOME/.config}/wget/wgetrc"
 export GTK2_RC_FILES="${XDG_CONFIG_HOME:-$HOME/.config}/gtk-2.0/gtkrc-2.0"
