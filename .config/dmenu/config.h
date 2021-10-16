74f23012f59c32ee19f4ea1388aa609e5386f97f
Exyloss <antoninpons@protonmail.com>
2021-10-16 23:13:53 +0200

    commit

--- .config/dmenu/config.h
+++ .config/dmenu/config.h
@@ -11,13 +11,13 @@ static int min_width = 500;                    /* minimum width when centered */
 static const char *fonts[] = {"monospace:size=10", "JoyPixels:pixelsize=8:antialias=true:autohint=true"};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 // cyan = "#005577"
-// magenta = "#7a5ccc"
+// magenta = "#694bbb"
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#000000" },
 	[SchemeSel] = { "#eeeeee", "#694bbb" },
-	[SchemeSelHighlight] = { "#ffc978", "#694bbb" },
-	[SchemeNormHighlight] = { "#ffc978", "#000000" },
+	[SchemeSelHighlight] = { "#353c67", "#694bbb" },
+	[SchemeNormHighlight] = { "#686f9a", "#000000" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
