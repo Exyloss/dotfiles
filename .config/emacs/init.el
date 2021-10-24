e1d8cd703ace7fcaad99c8fb45efaf7601cb9929
Exyloss <antoninpons@protonmail.com>
2021-10-24 23:00:34 +0200

    ajout mpd

---
+++ .config/emacs/init.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(tango-dark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(add-to-list 'custom-theme-load-path "~/.config/emacs/themes")
+(add-to-list 'load-path "~/.config/emacs/evil")
+(require 'evil)
+(evil-mode 1)
+(load-theme 'spaceduck t)
